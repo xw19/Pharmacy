8279b1c71e900c8e9725d87be9af38ab2913b5c0
Sourav Moitra <sourav.moitr@gmail.com>
2015-03-16 15:11:11 +0530

    Validations for address line1 and line2

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,6 @@ class Customer < ActiveRecord::Base
 
   validates :name , presence: true, length: { maximum: 50 }
   validates :address_line1 , presence: true, length: { maximum: 150 }
+  validates :address_line2 , presence: true, length: { maximum: 150 }
 
 end
